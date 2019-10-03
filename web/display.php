b6a65cf532ba54bbcfb5ae40f5c9cd4091c2f52e
Cody <hat15008@byui.edu>
2019-10-02 21:21:02 -0600

    added week03 Team Assignment: ta03.php and displayTa03.php

--- web/display.php
+++ web/display.php
@@ -8,29 +8,29 @@ $textarea = $_POST['textarea'];
 $continents = $_POST['cont'];
 
 $continentMap = array(
-    'NA'=>"North America",
-    'SA'=>"South America",
-    'EU'=>"Europe",
-    'AS'=>"Asia",
-    'AU'=>"Australia",
-    'AF'=>"Africa",
-    'AN'=>"Antarctica",
+	'NA'=>"North America",
+	'SA'=>"South America",
+	'EU'=>"Europe",
+	'AS'=>"Asia",
+	'AU'=>"Australia",
+	'AF'=>"Africa",
+	'AN'=>"Antarctica",
 );
 
 ?>
 
 <!DOCTYPE html>
 <html>
-<body>
-    <?php
-    echo "<p>Your name: $name</p>";
-    echo "<p>Your email: <a href='mailto:$email'>$email</a></p>";
-    echo "<p>Your major: $major</p>";
-    echo "<p>Comments: $textarea</p>";
-    echo "<p>You have been to:</p>";
-    foreach($continents as $continent) {
-        echo "$continentMap[$continent]<br>";
-    };
-    ?>
-</body>
+	<body>
+		<?php
+		echo "<p>Your name: $name</p>";
+		echo "<p>Your email: <a href='mailto:$email'>$email</a></p>";
+		echo "<p>Your major: $major</p>";
+		echo "<p>Comments: $textarea</p>";
+		echo "<p>You have been to:</p>";
+		foreach($continents as $continent) {
+			echo "$continentMap[$continent]<br>";
+		};
+		?>
+	</body>
 </html>
--- web/ta03.php
+++ web/ta03.php
@@ -1,43 +1,43 @@
 <!DOCTYPE html>
 <html>
-<body>
-    <form method='post' action='display.php'>
-		
-        Name: <input type='text' name='person'><br>
-        Email: <input type='text' name='email'><br>
-        <p>Major:</p>
-		
-        <?php
-        $majors = array("Computer Science", "Web Design and Development", "Computer information Technology", "Computer Engineering");
-        foreach($majors as $major){
-            echo "<input type='radio' name='major' value='$major'>$major<br>";
-        }
-        ?>
-		
-        <br>
-		<p>Comments:</p>
-        <textarea rows='10' cols='50' name='textarea'></textarea>
-        <br>
-		
-        <p>What continents have you visited?</p>
-        <?php
-        $continentMap = array(
-            'NA'=>"North America",
-            'SA'=>"South America",
-            'EU'=>"Europe",
-            'AS'=>"Asia",
-            'AU'=>"Australia",
-            'AF'=>"Africa",
-            'AN'=>"Antarctica",
-        );
+	<body>
+		<form method='post' action='display.php'>
 
-        $continents = array('NA', 'SA', 'EU', 'AS', 'AU', 'AF', 'AN');
-        foreach($continents as $continent) {
-            echo "<input type='checkbox' name='cont[]' value='$continent'>$continentMap[$continent]<br>";
-        };
-        ?>
+			Name: <input type='text' name='person'><br>
+			Email: <input type='text' name='email'><br>
+			<p>Major:</p>
 
-        <input type='submit' name='submit' value='Submit'>
-    </form>
-</body>
+			<?php
+			$majors = array("Computer Science", "Web Design and Development", "Computer information Technology", "Computer Engineering");
+			foreach($majors as $major){
+				echo "<input type='radio' name='major' value='$major'>$major<br>";
+			}
+			?>
+
+			<br>
+			<p>Comments:</p>
+			<textarea rows='10' cols='50' name='textarea'></textarea>
+			<br>
+
+			<p>What continents have you visited?</p>
+			<?php
+			$continentMap = array(
+				'NA'=>"North America",
+				'SA'=>"South America",
+				'EU'=>"Europe",
+				'AS'=>"Asia",
+				'AU'=>"Australia",
+				'AF'=>"Africa",
+				'AN'=>"Antarctica",
+			);
+
+			$continents = array('NA', 'SA', 'EU', 'AS', 'AU', 'AF', 'AN');
+			foreach($continents as $continent) {
+				echo "<input type='checkbox' name='cont[]' value='$continent'>$continentMap[$continent]<br>";
+			};
+			?>
+
+			<input type='submit' name='submit' value='Submit'>
+		</form>
+	</body>
 </html>

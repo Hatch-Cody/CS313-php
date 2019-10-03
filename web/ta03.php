b9125b5b0e070640be96b7f70209c99f8a0ac1f5
Cody <hat15008@byui.edu>
2019-10-02 21:17:58 -0600

    added week03 Team Assignment: ta03.php and displayTa03.php

--- web/ta03.php
+++ web/ta03.php
@@ -2,20 +2,24 @@
 <html>
 <body>
     <form method='post' action='display.php'>
+		
         Name: <input type='text' name='person'><br>
         Email: <input type='text' name='email'><br>
         <p>Major:</p>
+		
         <?php
         $majors = array("Computer Science", "Web Design and Development", "Computer information Technology", "Computer Engineering");
         foreach($majors as $major){
             echo "<input type='radio' name='major' value='$major'>$major<br>";
         }
         ?>
+		
         <br>
-        <textarea rows='20' cols='100' name='textarea'></textarea>
+		<p>Comments:</p>
+        <textarea rows='10' cols='50' name='textarea'></textarea>
         <br>
-        <p>Where you been bruh?</p>
-
+		
+        <p>What continents have you visited?</p>
         <?php
         $continentMap = array(
             'NA'=>"North America",
@@ -33,7 +37,7 @@
         };
         ?>
 
-        <input type='submit' name='submit' value='Send me away!'>
+        <input type='submit' name='submit' value='Submit'>
     </form>
 </body>
 </html>

76ff29a90143301664e4077bf46f3c36833d6b87
Cody <hat15008@byui.edu>
2019-10-17 15:46:56 -0600

    changed styling for assignment page

--- web/assignments.php
+++ web/assignments.php
@@ -1,57 +1,73 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>Personal Homepage</title> 
 
-		<meta charset = "utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+	<title>Personal Homepage</title>
 
-		<link rel="stylesheet" href="aboutMe.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	</head>
+	<link rel="stylesheet" href="aboutMe.css">
 
-	<body>
+</head>
 
-		<!-- Nav Bar -->
-		<div class="topnav">
-			<a href="home.php">Home</a>
-			<a class="active" href="assignments.php">Assignments</a>
-			<a href="intro.html">About Me</a>
+<body>
+
+	<!-- Nav Bar -->
+	<div class="topnav">
+		<a href="home.php">Home</a>
+		<a class="active" href="assignments.php">Assignments</a>
+		<a href="intro.html">About Me</a>
+	</div>
+
+	<div class="page">
+
+		<h1 style="text-align: center">CS313 Assignments</h1>
+
+		<div class="flexContainer">
+
+			<a href="intro.html" class="wideButton">Assignment # 1</a>
+
+			<a href="#assignment2" class="wideButton">Assignment # 2</a>
+
+			<a href="browseItems.php" class="wideButton">Assignment # 3</a>
+
 		</div>
 
-		<div class="page">
-				
-			<h1 style="text-align: center">CS313 Assignments</h1>
+		<div class="flexContainer">
 
-			<div class="flexContainer">
+			<a href="" class="wideButton">Assignment # 4</a>
 
-				<a href="intro.html" class="wideButton">Assignment # 1</a> 
+			<a href="" class="wideButton">Assignment # 5</a>
 
-				<a href="#assignment2" class="wideButton">Assignment # 2</a>
+			<a href="" class="wideButton">Assignment # 6</a>
 
-				<a href="browseItems.php" class="wideButton">Assignment # 3</a>
-
-				<a href="" class="wideButton">Assignment # 4</a> 
-
-				<a href="" class="wideButton">Assignment # 5</a>
-
-				<a href="" class="wideButton">Assignment # 6</a>
-
-				<a href="" class="wideButton">Assignment # 7</a> 
-
-				<a href="" class="wideButton">Assignment # 8</a>
-
-				<a href="" class="wideButton">Assignment # 9</a>
-
-				<a href="" class="wideButton">Assignment # 10</a>
-
-				<a href="projectHome.html" class="wideButton">Personal Project</a>
-
-			</div>
-
-			<footer class="footer">
-				Page Designed By: Cody Hatch
-			</footer>
 		</div>
-	</body>
+
+		<div class="flexContainer">
+
+			<a href="" class="wideButton">Assignment # 7</a>
+
+			<a href="" class="wideButton">Assignment # 8</a>
+
+			<a href="" class="wideButton">Assignment # 9</a>
+
+		</div>
+
+		<div class="flexContainer">
+
+			<a href="" class="wideButton">Assignment # 10</a>
+
+			<a href="projectHome.html" class="wideButton">Personal Project</a>
+			
+		</div>
+
+	</div>
+
+	<footer class="footer">
+		Page Designed By: Cody Hatch
+	</footer>
+	</div>
+</body>
+
 </html>

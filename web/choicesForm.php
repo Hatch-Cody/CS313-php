e5636d81764dfe39c1bd9896483f0a2a9b2ae16e
Cody <hat15008@byui.edu>
2019-10-19 11:42:07 -0600

    changed styling for choiceResults.php

--- web/choicesForm.php
+++ web/choicesForm.php
@@ -1,5 +1,5 @@
 <?php
-    include_once 'includes/dbh.php';
+include_once 'includes/dbh.php';
 ?>
 
 <!DOCTYPE html>
@@ -25,7 +25,9 @@
 			// show snackbar alert that the choices have been sent
 			var x = document.getElementById("snackbar");
 			x.className = "show";
-			setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+			setTimeout(function() {
+				x.className = x.className.replace("show", "");
+			}, 3000);
 
 			nav();
 
@@ -33,11 +35,54 @@
 		}
 
 		function nav() {
-            // wait two seconde then navigate to new page
-            window.location.assign("choiceResult.php");
-        }
-
+			// wait two seconde then navigate to new page
+			window.location.assign("choiceResult.php");
+		}
 	</script>
+
+	<style>
+		#snackbar {
+			visibility: hidden;
+			min-width: 250px;
+			margin-left: -125px;
+			background-color: #333;
+			color: #fff;
+			text-align: center;
+			border-radius: 2px;
+			padding: 16px;
+			position: fixed;
+			z-index: 1;
+			left: 50%;
+			bottom: 30px;
+			font-size: 17px;
+		}
+
+		#snackbar.show {
+			visibility: visible;
+			-webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
+			animation: fadein 0.5s, fadeout 0.5s 2.5s;
+		}
+
+		@-webkit-keyframes fadein {
+			from { bottom: 0; opacity: 0; }
+			to { bottom: 30px; opacity: 1; }
+		}
+
+		@keyframes fadein {
+			from { bottom: 0; opacity: 0; 
+			to { bottom: 30px; opacity: 1; }
+		}
+
+		@-webkit-keyframes fadeout {
+			from { bottom: 30px; opacity: 1; }
+			to { bottom: 0; opacity: 0; }
+		}
+
+		@keyframes fadeout {
+			from { bottom: 30px; opacity: 1; }
+			to { bottom: 0; opacity: 0 }
+		}
+	</style>
 
 </head>
 
@@ -60,11 +105,13 @@
 					<input type="text" id="least" placeholder="Least Favorite">
 					<br>
 					<input type="submit" onclick="submitChoices()">
-					
-					<div class="snackbar" id="snackbar">Choices have been submitted</div>
+
+
 				</form>
 			</div>
 		</div>
+
+		<div class="snackbar" id="snackbar">Choices have been submitted</div>
 
 		<footer class="footer">
 			Page Designed By: Cody Hatch

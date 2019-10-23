55f5b377118c4f2c16c9e0aaed2a3bafa8cb3ffe
Cody <hat15008@byui.edu>
2019-10-23 16:01:23 -0600

    uploaded files for week06 team assignment

--- web/teamAssignments/write.php
+++ web/teamAssignments/write.php
@@ -1,53 +1,47 @@
 <?php
 
-  require 'databaseConnect.php';
-  $db = get_db();
+$book = $_POST['book'];
+$chapter = $_POST['chapter'];
+$verse = $_POST['verse'];
+$content = $_POST['content'];
+$topicIds = $_POST['chkTopics'];
 
-  $book=$_POST['book'];
-  $chapter=$_POST['chapter'];
-  $verse=$_POST['verse'];
-  $content=$_POST['content'];
-  $topicIds = $_POST['chkTopics'];
+require 'databaseConnect.php';
+$db = get_db();
 
-  try 
-  {
-    // prepare query with placeholders
-    $query = 'INSERT INTO scripture(book, chapter, verse, content) VALUES(:book, :chapter, :verse, :content)';
-    $statement = $db->prepare($query);
+try {
+  // prepare query with placeholders
+  $query = 'INSERT INTO scripture(book, chapter, verse, content) VALUES(:book, :chapter, :verse, :content)';
+  $statement = $db->prepare($query);
 
-    // bind values to the placeholders
-    $statement->bindValue(':book', $book);
-    $statement->bindValue(':chapter', $chapter);
-    $statement->bindValue(':verse', $verse);
-    $statement->bindValue(':content', $content);
+  // bind values to the placeholders
+  $statement->bindValue(':book', $book);
+  $statement->bindValue(':chapter', $chapter);
+  $statement->bindValue(':verse', $verse);
+  $statement->bindValue(':content', $content);
+
+  $statement->execute();
+
+  $scriptureId = $db->lastInsertId("scripture_id_seq");
+
+  foreach ($topicIds as $topicId) {
+    echo "ScriptureId: $scriptureId, topicId: $topicId";
+
+    // prepare query for scriptureTopic
+    $statement = $db->prepare('INSERT INTO scripture_topic(scriptureId, topicId) VALUES(:scriptureId, :topicId)');
+
+    // bind values to placeholders
+    $statement->bindValue(':scriptureId', $csriptureId);
+    $statement->bindValue(':topicId', $topicId);
 
     $statement->execute();
-
-    $scriptureId = $db->lastInsertId("scripture_id_seq");
-
-    foreach ($topicIds as $topicId)
-    {
-      echo "ScriptureId: $scriptureId, topicId: $topicId";
-
-      // prepare query for scriptureTopic
-      $statement = $db->prepare('INSERT INTO scripture_topic(scriptureId, topicId) VALUES(:scriptureId, :topicId)');
-
-      // bind values to placeholders
-      $statement->bindValue(':scriptureId', $csriptureId);
-      $statement->bindValue(':topicId', $topicId);
-
-      $statement->execute();
-    }
   }
-  catch (Exception $ex)
-  {
-    echo "Error with database: $ex";
-    die();
-  }
-
-  // redirect to showTopics page
-  header("Location: showTopics.php");
-  
+} catch (Exception $ex) {
+  echo "Error with database: $ex";
   die();
-  
-?>
+}
+
+// redirect to showTopics page
+header("Location: showTopics.php");
+
+die();

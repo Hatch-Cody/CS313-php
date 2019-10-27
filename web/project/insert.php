e5b0e21d708e01534c7d0645538ccaec557bba40
Cody <hat15008@byui.edu>
2019-10-27 13:52:09 -0600

    adding php to insert data into the database

--- web/project/insert.php
+++ web/project/insert.php
@@ -12,8 +12,39 @@ date_default_timezone_set("America/Denver");
 require("dbConnect.php");
 $db = get_db();
 
-try
-{
+try {
+
+   $query = 'INSERT INTO "group_member" (group_id, choice_one, choice_two, choice_three, least_favorite, username, date) VALUES(:group_id, :num_one, :num_two, :num_three, :least_favorite :username :date)';
+   $statement = $db->prepare($query);
+   $statement->bindValue(':group_id',       $group_id);
+   $statement->bindValue(':num_one',        $num_one);
+   $statement->bindValue(':num_two',        $num_two);
+   $statement->bindValue(':num_three',      $num_three);
+   $statement->bindValue(':least_favorite', $least_favorite);
+   $statement->bindValue(':username',       $username);
+   $statement->execute();
+
+   // Now we bind the values to the placeholders. This does some nice things
+   // including sanitizing the input with regard to sql commands.
+} catch (Exception $ex) {
+   // Please be aware that you don't want to output the Exception message in
+   // a production environment
+   echo "Error with DB. Details: $ex";
+   die();
+}
+
+// finally, redirect them to a new page to actually show the topics
+header("Location: choiceResult.php");
+
+die(); // we always include a die after redirects. In this case, there would be no
+       // harm if the user got the rest of the page, because there is nothing else
+       // but in general, there could be things after here that we don't want them
+       // to see.
+
+
+
+
+       
    // $query = 'UPDATE group
    // SET (num_one, num_two, num_three, least_favorite, username, group_id, date) = (:num_one :num_two :num_three :least_favorite :username :group_id :date) 
    // WHERE group_id = "$group_id"';
@@ -27,34 +58,3 @@ try
    // $statement->bindValue(':group_id', $group_id);
    // $statement->bindValue(':date', '\'now()\'');
    // $statement->execute();
-
-   $query = 'INSERT INTO "group_member" (group_id, choice_one, choice_two, choice_three, least_favorite, username, date) VALUES (:group_id, :num_one :num_two :num_three :least_favorite :username :date)';
-    $statement = $db->prepare($query);
-    $statement->bindValue(':group_id',       $group_id);
-    $statement->bindValue(':num_one',        $num_one);
-    $statement->bindValue(':num_two',        $num_two);
-    $statement->bindValue(':num_three',      $num_three);
-    $statement->bindValue(':least_favorite', $least_favorite);
-    $statement->bindValue(':username',       $username);
-    $statement->execute();
-
-	// Now we bind the values to the placeholders. This does some nice things
-	// including sanitizing the input with regard to sql commands.
-}
-catch (Exception $ex)
-{
-	// Please be aware that you don't want to output the Exception message in
-	// a production environment
-	echo "Error with DB. Details: $ex";
-	die();
-}
-
-// finally, redirect them to a new page to actually show the topics
-header("Location: choiceResult.php");
-
-die(); // we always include a die after redirects. In this case, there would be no
-       // harm if the user got the rest of the page, because there is nothing else
-       // but in general, there could be things after here that we don't want them
-       // to see.
-
-?>

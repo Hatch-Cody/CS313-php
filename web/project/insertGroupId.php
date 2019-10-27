42d9782e37919cebc04c6e2932af3871bb9ff321
Cody <hat15008@byui.edu>
2019-10-27 11:13:45 -0600

    adding php to insert data into the database

--- web/project/insertGroupId.php
+++ web/project/insertGroupId.php
@@ -2,8 +2,29 @@
 // get the data from the POST
 $group_id = $_POST['group_id'];
 
-require("databaseConnect.php");
-$db = get_db();
+
+$db;
+
+try {
+    $dbUrl = getenv('DATABASE_URL');
+
+    $dbOpts = parse_url($dbUrl);
+
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"], '/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} 
+catch (PDOException $ex) 
+{
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+}
 
 try
 {
@@ -30,5 +51,3 @@ die(); // we always include a die after redirects. In this case, there would be 
        // harm if the user got the rest of the page, because there is nothing else
        // but in general, there could be things after here that we don't want them
        // to see.
-
-?>

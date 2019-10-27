3ae3ce856f53ae92c1a71c5b3b9fb0fdf4521d1a
Cody <hat15008@byui.edu>
2019-10-27 14:46:53 -0600

    adding php to insert data into the database

--- web/project/choiceResult.php
+++ web/project/choiceResult.php
@@ -1,8 +1,9 @@
 <?php
-    $group_id = $_GET['group_id'];
+include_once 'includes/dbh.php';
+?>
 
-    require("dbConnect.php");
-    $db = get_db();
+<?php
+$group_id = $_GET['group_id'];
 ?>
 
 <!DOCTYPE html>
@@ -31,6 +32,24 @@
 
 
                     <?php
+                    try {
+                        $dbUrl = getenv('DATABASE_URL');
+
+                        $dbOpts = parse_url($dbUrl);
+
+                        $dbHost = $dbOpts["host"];
+                        $dbPort = $dbOpts["port"];
+                        $dbUser = $dbOpts["user"];
+                        $dbPassword = $dbOpts["pass"];
+                        $dbName = ltrim($dbOpts["path"], '/');
+
+                        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+                        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    } catch (PDOException $ex) {
+                        echo 'Error!: ' . $ex->getMessage();
+                        die();
+                    }
 
                     $statement = $db->query('SELECT 
                     username, 

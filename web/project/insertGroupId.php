077e76c60b9153eb0e044b8b7dd6fcf001bea2ca
Cody <hat15008@byui.edu>
2019-10-27 13:22:24 -0600

    adding php to insert data into the database

--- web/project/insertGroupId.php
+++ web/project/insertGroupId.php
@@ -28,7 +28,7 @@ try {
 }
 
 // finally, redirect them to a new page to actually show the topics
-//header("Location: choicesForm.php?group_id=$group_id");
+header("Location: choicesForm.php?group_id=$group_id");
 
 die(); // we always include a die after redirects. In this case, there would be no
        // harm if the user got the rest of the page, because there is nothing else

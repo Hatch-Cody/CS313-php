f520edbc8d878c522d5097b4089117e2d4f06573
Cody <hat15008@byui.edu>
2019-10-27 01:19:50 -0600

    adding php to insert data into the database

--- web/teamAssignments/databaseConnect.php
+++ web/teamAssignments/databaseConnect.php
@@ -3,18 +3,23 @@
 function get_db()
 {
 	try {
-		$dbUrl  = getenv('DATABASE_URL');
+		$dbUrl = getenv('DATABASE_URL');
+
 		$dbOpts = parse_url($dbUrl);
-		$dbHost     = $dbOpts["host"];
-		$dbPort     = $dbOpts["port"];
-		$dbUser     = $dbOpts["user"];
+
+		$dbHost = $dbOpts["host"];
+		$dbPort = $dbOpts["port"];
+		$dbUser = $dbOpts["user"];
 		$dbPassword = $dbOpts["pass"];
 		$dbName = ltrim($dbOpts["path"], '/');
+
 		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	} catch (PDOException $ex) {
 		echo 'Error!: ' . $ex->getMessage();
 		die();
 	}
+
 	return $db;
 }

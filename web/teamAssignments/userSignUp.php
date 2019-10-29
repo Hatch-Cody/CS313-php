b5151487bd34019ae26e73eea84b6c54dcc03ca0
Cody <hat15008@byui.edu>
2019-10-29 16:13:00 -0600

    adding team assign 07

--- web/teamAssignments/userSignUp.php
+++ web/teamAssignments/userSignUp.php
@@ -13,10 +13,10 @@
 <body>
     <div class="body">
         <div class="loginBox">
-            <h1> Register </h1>
+            <h1>Register</h1>
             <form action="signUp.php" method="POST">
-                <input type="text" placeholder="username"><br>
-                <input type="password" placeholder="password"><br>
+                <input type="text" name= "username" placeholder="username"><br>
+                <input type="password" name="password" placeholder="password"><br>
                 <br>
 
                 <input type="submit" value="submit">

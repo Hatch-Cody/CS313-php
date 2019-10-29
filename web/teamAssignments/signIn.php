1b33b2075c23f70d78cccfdad0a0c30c6bba812d
Cody <hat15008@byui.edu>
2019-10-29 16:25:51 -0600

    adding team assign 07

--- web/teamAssignments/signIn.php
+++ web/teamAssignments/signIn.php
@@ -9,7 +9,7 @@ $password = $_POST['password'];
 //echo "Username: $username, Password: $password";
 
 
-try {
+try{
 $query = 'SELECT password FROM "user1" WHERE username = :username';
 $statement = $db->prepare($query);
 $statement->bindValue(':username', $username);
@@ -17,15 +17,15 @@ $statement->execute();
 
 $hashedPassword = $statement->fetch(PDO::FETCH_ASSOC);
 echo "HASH: $hashedPassword";
-/*
-if (password_verify($password, $hashedPassword))
-{
-    header('Location: welcomeUser.php');
-}
+
+// if (password_verify($password, $hashedPassword))
+// {
+//     header('Location: welcomeUser.php');
+// }
 }catch (Exception $ex) {
     echo "Error with DB. Details: $ex";
 	die();
 }
-*/
+
 
 ?>

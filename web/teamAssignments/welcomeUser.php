71121de41b3ba059bb8748b8f307c2a5418e12f4
Cody <hat15008@byui.edu>
2019-10-30 07:50:40 -0600

    adding team assign 07

--- web/teamAssignments/welcomeUser.php
+++ web/teamAssignments/welcomeUser.php
@@ -6,16 +6,16 @@
     <link rel="stylesheet" type="text/css" href="loginStyle.css">
 
     <title>Welcome Users</title>
+    
+</head>
+
+<body class="body">
 
     <?php
 
     $username = $_GET['username'];
     echo "<h1>Welcome $username</h1>";
     ?>
-
-</head>
-
-<body class="body">
 
 </body>
 

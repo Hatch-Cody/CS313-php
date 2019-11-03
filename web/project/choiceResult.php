c04d1a1901e67c217375141119a03cacf38bfd44
Cody <hat15008@byui.edu>
2019-11-03 00:02:21 -0600

    adding about.php to the project

--- web/project/choiceResult.php
+++ web/project/choiceResult.php
@@ -94,7 +94,7 @@ $group_id = $_GET['group_id'];
                     //     }
                     // }
 
-                    echo 'Results: ' . key($choices[0]) . ' | ' . key($choices[1]) . ' | ' . key($choices[2]) . '<br>';
+                    echo '<br>Results: ' . key($choices[0]) . ' | ' . key($choices[1]) . ' | ' . key($choices[2]) . '<br>';
 
 
 

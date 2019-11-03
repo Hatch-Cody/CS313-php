8f9976920e824517c50c8ad82fc791683ffd1a9c
Cody <hat15008@byui.edu>
2019-11-03 00:25:17 -0600

    adding about.php to the project

--- web/project/choiceResult.php
+++ web/project/choiceResult.php
@@ -68,6 +68,8 @@ $group_id = $_GET['group_id'];
                     $keys = array_keys($choices);
                     $keys2 = array_keys($leastFavorite);
 
+                    echo $choices;
+
                     echo $keys[0] . ' | ' . $keys[1] . ' | ' . $keys[2] . '<br><br><h2>Least Favorite</h2>' . $keys2[0] . '<br>';
 
                     ?>

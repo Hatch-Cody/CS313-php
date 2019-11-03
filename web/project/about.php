c2eef06499ee2a6ae07662afa02c11319434b8e2
Cody <hat15008@byui.edu>
2019-11-02 21:59:18 -0600

    adding about.php to the project

--- web/project/about.php
+++ web/project/about.php
@@ -25,20 +25,20 @@
             <div class="myModal-dialog">
                 <div class="myModal-content">
                     <div class="myModal-header">
-                        <h3 class="myModal-title"><b>Instructions</b></h3>
+                        <h3 class="myModal-title"><b>About</b></h3>
                         <button type="button" class="closeButton">
                             <span>&times;</span>
                         </button>
                     </div>
                     <div class="myModal-body">
-                        <p>
+                        <p style="font-family: 'Roboto', sans-serif;">
                             Have you ever been with a group of friends or family as they try to decide where they should go out to eat?
                             Of course you have! We all have! It is a battle as each person struggles to voice their opinion about which
                             place would be best or which places are a bad choice. It can get pretty chaotic, especially when you consider
-                            they are all hangry.<br>
+                            they are all hangry.<br><br>
 
                             Group Choice was made to solve this problem so that you and your friends or family can avoid the brawl and get
-                            on with your lives.<br>
+                            on with your lives.<br><br>
 
                             This app was designed with simplicity in mind: All you have to do is make or join a group and input your top
                             three restaurant choices and if you have a restaurant that you dont like, put that in there too. Then all you
